0f8b8ff7c886a642378420fec1db3dd7bd6ba13e
s2402046-pixel <s2402046@usls.edu.ph>
2026-08-14 01:19:10 +0800

    Add files via upload

---
+++ Hobbies.php
@@ -0,0 +1,28 @@
+<?php
+    $title = "Matthew Gelera - Hobbies";
+    $header = "My Hobbies and Interests";
+?>
+
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+
+        <h1><?php echo $header; ?></h1>
+
+        <p>
+            Some of my hobbies and interests include playing video games,
+            programming, playing sports, listening to music, and learning
+            about game development. I also enjoy playing badminton and Ultimate Frisbee.
+        </p>
+
+        <h2>Navigation</h2>
+
+        <a href="index.php">Home</a><br>
+        <a href="about.php">About Me</a><br>
+        <a href="hobbies.php">My Hobbies</a>
+
+    </body>
+</html>
---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+    $title = "Matthew Gelera - About Me";
+    $header = "About Me";
+?>
+
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+
+        <h1><?php echo $header; ?></h1>
+
+        <p>
+            My name is Matthew Gabrielle D. Gelera. I am a Computer Science
+            student who is interested in programming, technology, and game
+            development. I enjoy learning new skills and creating projects
+            using computers.
+        </p>
+
+        <h2>Navigation</h2>
+
+        <a href="index.php">Home</a><br>
+        <a href="about.php">About Me</a><br>
+        <a href="hobbies.php">My Hobbies</a>
+
+    </body>
+</html>
