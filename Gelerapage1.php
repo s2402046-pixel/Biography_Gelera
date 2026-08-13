47b1dba9ff89384b559d59ea369a9cef6473c649
s2402046-pixel <s2402046@usls.edu.ph>
2026-08-14 01:33:46 +0800

    Add files via upload

---
+++ Gelerapage1.php
@@ -0,0 +1,28 @@
+<?php
+    $title = "Matthew Gelera - Home";
+    $header = "Welcome to My Portfolio";
+?>
+
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+
+    <body>
+
+        <h1><?php echo $header; ?></h1>
+
+        <p>
+            Welcome to my simple portfolio website. 
+            This website contains information about me, my hobbies,
+            and some of my interests.
+        </p>
+
+        <h2>Navigation</h2>
+
+        <a href="Gelerapage1.php">Home</a><br>
+        <a href="Gelerapage2.php">About Me</a><br>
+        <a href="Gelerapage3.php">My Hobbies</a>
+
+    </body>
+</html>
